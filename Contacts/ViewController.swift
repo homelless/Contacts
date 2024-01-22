0f8b0ef47aa7f7ad2ea3d0e875365350c01c5307
homeless <hmlssgd@gmail.com>
2024-01-22 19:21:08 +0300

    7.2

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -17,3 +17,20 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //получаем экземпляр ячейки
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        //конфигурируем ячейку
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = "Строка \(indexPath.row)"
+        cell.contentConfiguration = configuration
+        
+        // возвращаем сконфигурированый экземпляр ячейки
+        return cell
+    }
+}
